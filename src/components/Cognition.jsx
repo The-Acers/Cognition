3abe8b4e8ba74602e368dd61c23e42f8ca1d9bae
unknown <tatharv597@gmail.com>
2025-07-11 08:51:28 +0530

    Sponsor Addition

--- src/components/Cognition.jsx
+++ src/components/Cognition.jsx
@@ -89,11 +89,11 @@ export default function Cognition() {
 
           <a
             className="mt-6 inline-block bg-white text-yellow-600 hover:bg-gradient-to-r hover:from-yellow-500 hover:to-yellow-800 hover:text-white px-5 py-3 rounded-lg text-sm md:text-lg font-bold transition-colors duration-500 hover:shadow-lg"
-            href="//forms.gle/zTYsrLw2VwotBF367"
+            href="/"
           >
             Join The Campaign!! <br />
             <span className="text-sm text-gray-600 text-center hover:text-black">
-              (FOR FREE PASSES)
+              (CLOSED)
             </span>
           </a>
         </div>
